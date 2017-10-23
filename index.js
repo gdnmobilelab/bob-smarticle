5b1d62537c915b7bcb7a77b28bca6ab2fb30b5d2
sammorrisdesign <mail@sammorrisdesign.co.uk>
2017-10-23 15:10:25 -0400

    display data

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+var getData = require('./scripts/data.js');
+
 var express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send('HEY!')
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(getData()));
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'))
