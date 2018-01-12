ade29d96831fa64de627536c7bb0fda2a3b5c67b
sammorrisdesign <mail@sammorrisdesign.co.uk>
2018-01-12 15:11:14 -0500

    evaulate group faqs once groups have been formed

--- scripts/request.js
+++ scripts/request.js
@@ -1,13 +1,13 @@
 var fetch = require('./fetch.js');
 
 var cleanParams = require('./request/params.js');
-var additionalProperties = require('./request/additionalProperties.js');
 var notified = require('./request/notified.js');
 var seen = require('./request/seen.js');
 var rate = require('./request/rate.js');
 var clean = require('./request/clean.js');
 var cap = require('./request/cap.js');
 var groups = require('./request/groups.js');
+var additionalProperties = require('./request/additionalProperties.js');
 
 var fs = require('fs-extra');
 var bodyParser = require('body-parser');
@@ -25,13 +25,13 @@ app.post('/', (req, res) => {
     var data = params.debug ? fetch(params.id, params.debug) : fs.readJsonSync('./.data/smarticles/' + params.id + '.json');
         params = params.preview ? data.preview[params.preview] : params;
         params = cleanParams(params);
-        data = additionalProperties(data);
         data = notified(data, params.notified);
         data = seen(data, params);
         data = rate(data, params);
         data = clean(data);
         data = params.debug ? data : cap(data);
         data = groups(data, params);
+        data = additionalProperties(data);
 
     res.setHeader('Content-Type', 'application/json');
     res.header("Access-Control-Allow-Origin", "*");
