7cdf333706231a0aeca15248f7581a5f994c97e5
sammorrisdesign <mail@sammorrisdesign.co.uk>
2018-01-30 10:52:16 -0500

    add comma

--- scripts/request/onboarding.js
+++ scripts/request/onboarding.js
@@ -4,7 +4,7 @@ function addOnboardingAtom(data) {
         groupType: 'onboarding',
         atoms: {
             0: {
-                copy: ' The Guardian Mobile Innovation Lab is trying out a new mobile story format. This page will update with key developments about ' + data.furniture.title + ' over the next few days. When you return, you will only see the developments that are new or relevant to you, based on things such as when you last visited and the importance of each detail. Enjoy!'
+                copy: ' The Guardian Mobile Innovation Lab is trying out a new mobile story format. This page will update with key developments about ' + data.furniture.title + ' over the next few days. When you return, you will only see the developments that are new or relevant to you, based on things such as when you last visited and the importance of each detail. Enjoy!',
                 type: 'text'
             }
         }
