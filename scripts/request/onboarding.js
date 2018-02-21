93333d9b8cfc9b277ef44f89a3b554c517308ff7
sammorrisdesign <mail@sammorrisdesign.co.uk>
2018-02-21 10:05:31 -0500

    do not enjoy this

--- scripts/request/onboarding.js
+++ scripts/request/onboarding.js
@@ -4,7 +4,7 @@ function addOnboardingAtom(data) {
         groupType: 'onboarding',
         atoms: {
             0: {
-                copy: 'The Guardian Mobile Innovation Lab is trying out a new mobile story format. This page will update with key developments about ' + data.furniture.title + ' over the next few days. When you return, you will see the developments that are new or relevant to you, based on things such as when you last visited and the importance of each detail. We will also bring back certain elements of the story that you read previously to jog your memory and add context. Enjoy!',
+                copy: 'The Guardian Mobile Innovation Lab is trying out a new mobile story format. This page will update with key developments about ' + data.furniture.title + ' over the next few days. When you return, you will see the developments that are new or relevant to you, based on things such as when you last visited and the importance of each detail. We will also bring back certain elements of the story that you read previously to jog your memory and add context.',
                 type: 'text'
             }
         }
